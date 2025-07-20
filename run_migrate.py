6e451b0acbdb2bd3790cd6cb7b1e77474ce630e8
Khueminh2025 <khueminh2025@gmail.com>
2025-07-20 10:59:02 +0700

    super

--- run_migrate.py
+++ run_migrate.py
@@ -1,6 +1,7 @@
 import os
 import django
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
 # ✅ Chỉ định settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sieutoc_printing.settings')
@@ -10,3 +11,12 @@ django.setup()
 
 # ✅ Gọi migrate
 call_command('migrate')
+
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("sieutoc", "sieutocndc@example.com", "St134b@!")
+    print("✅ Superuser 'sieutoc' created with password ''")
+else:
+    print("✅ Superuser already exists")
