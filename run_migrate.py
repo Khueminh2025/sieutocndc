fa43f1d2c4d3a19922a328cff1b7d555811245c6
Khueminh2025 <khueminh2025@gmail.com>
2025-07-20 15:24:12 +0700

    Add loaddata auto for Render Free deploy

--- run_migrate.py
+++ run_migrate.py
@@ -12,8 +12,14 @@ django.setup()
 # ✅ Gọi migrate
 call_command('migrate')
 
+# Chạy loaddata nếu dữ liệu chưa có
+from printing.models import ServiceCategory
+if not ServiceCategory.objects.exists():
+    call_command("loaddata", "fixtures_sieutoc_printing_clean.json")
+    print("✅ Đã load dữ liệu JSON lên Render.")
 
-User = get_user_model()
+
+# User = get_user_model()
 
 # if not User.objects.filter(username="admin").exists():
 #     User.objects.create_superuser("sieutoc", "sieutocndc@example.com", "St134b@!")
