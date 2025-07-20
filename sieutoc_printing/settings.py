7db408ac19197c1c8e289bcbd71f5181d5630287
Khueminh2025 <khueminh2025@gmail.com>
2025-07-20 17:28:04 +0700

    met qua

--- sieutoc_printing/settings.py
+++ sieutoc_printing/settings.py
@@ -143,6 +143,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dzprogp4e',
+    'API_KEY': '636593968486674',
+    'API_SECRET': 'EqcsJjbTt3MSoLi8FZFP_a0MaFM'
+}
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
