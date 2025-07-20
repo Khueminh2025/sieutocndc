c43c3c4f7b4dd0f94f9ff04bc6b88f87d1b28681
Khueminh2025 <khueminh2025@gmail.com>
2025-07-20 10:52:27 +0700

    Ép migrate trong startCommand

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sieutoc_printing.wsgi
--- run_migrate.py
+++ run_migrate.py
@@ -1,5 +1,12 @@
-# run_migrate.py
+import os
+import django
 from django.core.management import call_command
 
-if __name__ == "__main__":
-    call_command('migrate')
+# ✅ Chỉ định settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sieutoc_printing.settings')
+
+# ✅ Setup Django thủ công
+django.setup()
+
+# ✅ Gọi migrate
+call_command('migrate')
